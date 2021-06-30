ba875777facc056931876dc2f83641b9ab217c74
yaish94 <mayaish94@gmail.com>
2021-06-30 21:11:27 +0300

    Update test_constrained_min.py

--- tests/test_constrained_min.py
+++ tests/test_constrained_min.py
@@ -9,14 +9,15 @@ from tests.examples import eq_constraints_mat_qp, eq_constraints_rhs_qp, objecti
     c2_qp, \
     c1_lp, c2_lp, c3_lp, c4_lp
 
-line_search_types = ["gd", "bfgs", "nt"]
-
 
 class TestConstrainedMin(unittest.TestCase):
 
     def setUp(self):
         plt.cla()
 
+    """
+     quadratic programming example
+    """
     def test_qp(self):
         x, history = interior_pt(objective_qp, np.array([c1_qp, c2_qp, c3_qp]),
                                  eq_constraints_mat_qp, eq_constraints_rhs_qp,
@@ -24,6 +25,10 @@ class TestConstrainedMin(unittest.TestCase):
 
         plot_feasible_region_qp(history)
 
+
+    """
+     linear programming example
+    """
     def test_lp(self):
         x, history = interior_pt(objective_lp, np.array([c1_lp, c2_lp, c3_lp, c4_lp]),
                                  None, None,
